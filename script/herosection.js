c2814917d3bd599eb834acb4d7184fb8b832d2dc
tarun <tarunkumar8059@gmail.com>
2023-03-04 11:40:45 +0530

    add new page of lookism

---
+++ script/herosection.js
@@ -0,0 +1,27 @@
+const herosection=document.getElementById('hero');
+const herohtml=`
+
+  <div id="feature"><a href="/episode page/lookism-all-episodes.html">
+    <img src="https://sm.ign.com/t/ign_in/screenshot/default/lookism_r6n7.1280.jpg"
+      alt="lookism" />
+    <h2>hello this is title</h2></a>
+  </div>
+  <div class="slide">
+    <div class="sl1"><a href="/single-page/pokemon-all-seasons.html">
+      <img src="../src/pokemonallseason.png"
+        alt="" />
+      <h2>hello this is title</h2></a>
+    </div>
+    <div class="sl"><a href="/single-page/pokemon-all-movies.html">
+      <img src="../src/pokemonallseason.png"
+        alt="" />
+      <h2>hello this is title</h2></a>
+    </div>
+    <div class="sl"><a href="/">
+      <img src="../src/pokemonallseason.png"
+        alt="" />
+      <h2>hello this is title</h2></a>
+    </div>
+  </div>
+`;
+herosection.innerHTML=herohtml;
